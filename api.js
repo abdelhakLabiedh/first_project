30e3656fe0357dbb12acc0dc9d9b992b924ca4c1
unknown <abdelhak.labiedh@gmail.com>
2023-03-16 17:41:41 +0100

    first api server

---
+++ api.js
@@ -0,0 +1,10 @@
+const express=require('express')
+const app = express()
+
+
+
+app.use('',(req,res)=>{
+    res.json({message : 'salut'})
+})
+const port=3000
+app.listen(port,()=>console.log(`server done in ${port}`))
